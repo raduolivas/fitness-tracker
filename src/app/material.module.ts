4e56b99716a4e352a6a684b776cfe25c3b68084a
Rodolfo Silva <rodolfoslv@gmail.com>
2019-01-03 17:26:43 -0200

    Adition of routes and first form

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material';
-import { MatIconModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
 
 @NgModule({
-  imports:[MatButtonModule, MatIconModule],
-  exports:[MatButtonModule, MatIconModule]
+  imports:[MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  exports:[MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
 })
-export class MaterialModule {
 
-}
+export class MaterialModule {}
